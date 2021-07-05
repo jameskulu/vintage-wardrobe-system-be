9cbfa907a96837eb8dbaf36972bc8d59c1cf68c0
James Kulu <jameskulu55@gmail.com>
2021-07-05 17:14:13 +0545

    Confuguring prettierrc and eslist

--- server/server.js
+++ server/server.js
@@ -1,8 +1,22 @@
 const express = require('express');
+const morgan = require('morgan');
+const chalk = require('chalk');
+const cors = require('cors');
+
 const app = express();
+require('dotenv').config();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+app.use(morgan('tiny'));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+app.listen(PORT, () =>
+    console.log(
+        `${chalk.green('✓')} ${chalk.blue(
+            `Listening on http://localhost:${PORT}/.`
+        )}`
+    )
+);
