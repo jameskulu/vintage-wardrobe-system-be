cfe1ef3b3368feab87af4825b84bd354a07f044f
James Kulu <jameskulu55@gmail.com>
2021-07-05 17:55:48 +0545

    User validation integration

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.use(morgan('tiny'))
 const PORT = process.env.PORT || 5000
 
 db.sequelize
-    .sync({force:true})
+    .sync()
     .then(() =>
         app.listen(PORT, () =>
             console.log(
