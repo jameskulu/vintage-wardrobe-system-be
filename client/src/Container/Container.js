a07fb546ec88a3e503950024d15785ad672c2039
AnupShahi11 <shahirocks11@gmail.com>
2021-07-05 21:28:56 +0545

    Container Folder Created

---
+++ client/src/Container/Container.js
@@ -0,0 +1,14 @@
+import {Component} from "react";
+class Container extends Component{
+    render(){
+        return(
+            <div>
+                This is container
+            </div>
+        )
+    }
+
+    
+}
+
+export default Container;
