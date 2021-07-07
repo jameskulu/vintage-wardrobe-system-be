ee0ef0011607c8c10f54f580d6e696fe061ac61d
AnupShahi11 <shahirocks11@gmail.com>
2021-07-07 21:17:45 +0545

    Login page Design Added

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -3,26 +3,26 @@ import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
-    <div class="container">
-        <form action="">
-            <div class="login-form-container">
-                <h1>LOG IN</h1>
-
-
-                <div class="login-form">
-                 <input type="text" placeholder="Username" name="username" id="username" required/>
-                 <input type="password" placeholder="Password" name="pwd" id="pwd" required/>
-                 <a id='login-link' href="#">Forgot Password?</a>
-
-                 <button type="submit" class="loginbtn">LOGIN</button>
-               
-
-                <p>Don’t have an account? <Link to ="/Signup">Sign Up</Link>.</p>
-            </div>
-            </div>
-        </form>
-    </div>
-    );
+        <div class="container">
+            <form action="">
+                <div class="login-form-container">
+                    <h1>LOG IN</h1>
+    
+    
+                    <div class="login-form">
+                     <input type="text" placeholder="Username" name="username" id="username" required/>
+                     <input type="password" placeholder="Password" name="pwd" id="pwd" required/>
+                     <a id='login-link' href="#">Forgot Password?</a>
+    
+                     <button type="submit" class="loginbtn">LOGIN</button>
+                   
+    
+                    <p>Don’t have an account? <Link to ="/Signup">Sign Up</Link>.</p>
+                </div>
+                </div>
+            </form>
+        </div>
+        );
 };
 
 export default Login;
