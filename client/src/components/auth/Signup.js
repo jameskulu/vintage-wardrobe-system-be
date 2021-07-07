54aa11534cb0f25a9b75f5c8395f96642b67ad99
AnupShahi11 <shahirocks11@gmail.com>
2021-07-07 13:50:00 +0545

    register Functionalities added

--- client/src/components/auth/Signup.js
+++ client/src/components/auth/Signup.js
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useState } from "react";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
 
 const Signup = () => {
-    return <div>this is a Sign up page</div>;
+  const history = useHistory();
+
+  const [firstName, setFirstName] = useState();
+  const [lastName, setLastName] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  const userRegister = async (e) => {
+    e.preventDefault();
+    try {
+      const newUser = { firstName, lastName, email, password };
+      await axios.post("http://localhost:5000/api/users/register", newUser);
+
+      history.push("/login");
+    //   toast.success("Your account is created successfully.");
+    } catch (err) {
+    //   toast.error(err.response.data.msg);
+    }
+  };
+
+  return (
+    <div>
+        this is Signup page...
+     </div>
+  );
 };
 
 export default Signup;
