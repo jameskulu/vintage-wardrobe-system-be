b4fce9435660761b2b06e9e01d2e349e049a64f9
James Kulu <jameskulu55@gmail.com>
2021-07-08 11:29:39 +0545

    Initial Deploy

--- controllers/users.js
+++ controllers/users.js
@@ -2,7 +2,7 @@ const crypto = require('crypto')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const { User } = require('../models')
-const {
+const { 
     registerValidation,
     activateAccountValidation,
     loginValidation,
