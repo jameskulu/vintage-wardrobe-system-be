16036ee19b2a22de4a191e6371c4223249cf385f
ArbinChoudhary <arbin.choudhary789@gmail.com>
2021-07-21 12:50:19 +0545

    Added renter tdd testing

---
+++ test/renter/getordereditem.js
@@ -0,0 +1,25 @@
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+
+const server = require('../../server')
+
+chai.should()
+chai.use(chaiHttp)
+
+describe('Renter API', () => {
+
+    describe('GET /api/renter/orders', () => {
+        it("It should get ordered items",(done)=>{
+            chai.request(server)
+                .get("/api/renter/orders")
+                .end((err,response)=>{
+                    response.should.have.status(200)
+                    response.body.should.be.a('object')
+                    response.body.should.have.property('success')
+                    response.body.should.have.property('message')
+                    response.body.should.have.property('data')
+                    done()
+                })
+        })
+    })
+})
