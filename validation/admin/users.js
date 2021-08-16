e06a53fb3ea2ccf2cad2039768275e11cb453ee1
James Kulu <jameskulu55@gmail.com>
2021-08-16 20:52:49 +0545

    Added users admin crud routes

---
+++ validation/admin/users.js
@@ -0,0 +1,19 @@
+const Joi = require('@hapi/joi')
+
+exports.createValidation = (data) => {
+    const schema = Joi.object({
+        firstName: Joi.string().required(),
+        lastName: Joi.string().required(),
+        email: Joi.string()
+            .min(6)
+            .required()
+            .email({
+                minDomainSegments: 2,
+                tlds: { allow: ['com', 'net'] },
+            }),
+        password: Joi.string().min(6).required(),
+        role: Joi.string().valid('admin', 'user').required(),
+        gender: Joi.string().valid('ma', 'fe'),
+    })
+    return schema.validate(data)
+}
