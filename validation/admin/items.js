49e09e7bab4b05ace124bfdb60d84a3ea612e3c0
James Kulu <jameskulu55@gmail.com>
2021-08-18 22:51:54 +0545

    fixes

--- validation/admin/items.js
+++ validation/admin/items.js
@@ -5,8 +5,8 @@ exports.createValidation = (data) => {
         name: Joi.string().required(),
         description: Joi.string().required(),
         price: Joi.number().required(),
-        color: Joi.number().required(),
-        size: Joi.number().required(),
+        color: Joi.string().required(),
+        size: Joi.string().required(),
         subCategoryId: Joi.string().required(),
         userId: Joi.string().required(),
     })
